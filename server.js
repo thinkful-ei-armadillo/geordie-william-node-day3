1204fc70d0993787c073e84fb5aec1ebf2e04bce
William Y Wong <wwong1987@gmail.com>
2019-02-21 12:05:04 -0800

    added movies.json, fixed cors and helmet security issues

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const helmet = require('helmet');
+const cors = require('cors');
 const MOVIES = require('./movies.json');
 
 
@@ -13,12 +13,13 @@ console.log(process.env.API_TOKEN);
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors);
-app.use(helmet);
+// helmet is called before cors
+app.use(helmet());
+app.use(cors());
+
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  console.log(apiToken);
   const authToken = req.get('Authorization');
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
