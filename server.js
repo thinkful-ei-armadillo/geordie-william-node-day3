e54677e0cb328e562690a806a62ae2593b106be9
Geordie Connell <geordie.connell@gmail.com>
2019-02-21 12:19:01 -0500

    public security in place

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const MOVIES = require('./movies.json');
 
 
@@ -11,6 +13,8 @@ console.log(process.env.API_TOKEN);
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors);
+app.use(helmet);
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
